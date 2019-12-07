5c897256cf3655ce2411c1aef6c31850b2c32450
Swati Anand <swati.anand56@gmail.com>
2019-12-07 16:33:08 -0600

    added membership change policy based on the number of failures

---
+++ config_manager/config_manager_policy.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+	"time"
+)
+
+func isElementPresentInArray(arr []int, ele int) bool {
+	for _, i := range arr {
+		if i == ele {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	serverList := []string{
+		"localhost:8001",
+		"localhost:8002",
+		"localhost:8003",
+		"localhost:8004",
+		"localhost:8005",
+		"localhost:8006",
+		"localhost:8007",
+		"localhost:8008",
+		"localhost:8009",
+		"localhost:8010",
+		"localhost:8011",
+	}
+	activeServers := []int{0, 1, 2}
+	unreachableServers := []int{}
+	failureTestTime := 10 // time in seconds
+	failureHandleCapacity := 1
+	activeServersReqd := 3
+	const FailureCycles = 3
+	numFailureCycles := FailureCycles
+
+	for {
+		fmt.Println("sleeping for 10 seconds")
+		time.Sleep(time.Duration(failureTestTime) * time.Second)
+		// start with 3 machines: least number, can handle the failure of 1 machine (if a machine goes down -- up another machine)
+		// if n machines have failed in the last m minutes, increase failure tolerance by n+1 (max machines in our case = 11 -- this can tolerate the failure of 5 machines)
+		// if 0 machines have failed in the last m minutes, decrease failure tolerance by 1 (min number of machines being 3)
+		// ping all the active servers
+
+		fmt.Println("active servers are ---- ", activeServers, failureHandleCapacity, unreachableServers)
+
+		var serverStatus int
+		numFailures := 0
+
+		// make a check over unreachable servers and remove them from the list if they are now reachable
+
+		for index, server := range unreachableServers {
+			fmt.Println("checking unreachable server ", server)
+			if kv739_init([]string{serverList[server]}, 1) == 0 {
+				unreachableServers[index] = -1
+			}
+		}
+
+		sort.Slice(unreachableServers, func(i, j int) bool {
+			return unreachableServers[i] < unreachableServers[j]
+		})
+
+		for index, server := range unreachableServers {
+			if server == -1 {
+				unreachableServers = unreachableServers[:index]
+				break
+			}
+		}
+
+		for index, server := range activeServers {
+			fmt.Println("checking active server ", server)
+			serverStatus = kv739_init([]string{serverList[server]}, 1)
+			if serverStatus == -1 { // error connecting
+				unreachableServers = append(unreachableServers, server)
+				activeServers[index] = activeServers[len(activeServers)-1]
+				activeServers = activeServers[:len(activeServers)-1]
+				numFailures++
+				fmt.Println("failure encountered new active servers are ", activeServers, unreachableServers)
+			}
+		}
+		if numFailures == 0 {
+			if numFailureCycles == 0 {
+				failureHandleCapacity = int(math.Max(float64(failureHandleCapacity-1), float64(1)))
+				numFailureCycles = FailureCycles
+			} else {
+				numFailureCycles--
+			}
+		} else {
+			failureHandleCapacity = failureHandleCapacity + numFailures
+		}
+		activeServersReqd = (2 * failureHandleCapacity) + 1
+
+		fmt.Println("num failures, failure handle capacity, active servers required ", numFailures, failureHandleCapacity, activeServersReqd)
+
+		if activeServersReqd == len(activeServers) {
+			fmt.Println("No membership changes required")
+			continue
+		}
+
+		// Trigger config change if lengths do not match
+		if activeServersReqd > len(activeServers) {
+			numServersToAdd := activeServersReqd - len(activeServers)
+			for i := 0; i < len(serverList); i++ {
+				if numServersToAdd == 0 {
+					break
+				}
+				fmt.Println("checking server ", i)
+				if !isElementPresentInArray(activeServers, i) && !isElementPresentInArray(unreachableServers, i) {
+					fmt.Println("adding server", i, "to list of active servers")
+					activeServers = append(activeServers, i)
+					numServersToAdd--
+				}
+			}
+		} else if activeServersReqd < len(activeServers) {
+			activeServers = activeServers[:activeServersReqd]
+		}
+
+		if activeServersReqd != len(activeServers) {
+			fmt.Println("Servers required = ", activeServersReqd, "Actual active servers = ", len(activeServers))
+		}
+
+		fmt.Println("Membership changes ------ new active and unreachable servers are ", activeServers, unreachableServers)
+		// call config change on server
+		if kv739_changeMembership(activeServers) == -1 {
+			fmt.Println("Change membership is not successful to the given configuration ", activeServers)
+		} else {
+			fmt.Println("Change membership is successful to the given configuration", activeServers)
+		}
+	}
+}
---
+++ config_manager/stub.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var err error
+var client *rpc.Client
+var leaderIndex int
+var reply string
+var conn net.Conn
+var serverList []string
+
+//export kv739_init
+func kv739_init(serverListArg []string, length int) int {
+	serverList = serverListArg
+	leaderIndex = 0
+	if len(serverList) == 0 {
+		return -1
+	}
+	address := serverList[0]
+	conn, err = net.DialTimeout("tcp", address, 250*time.Millisecond)
+	if err != nil {
+		fmt.Println("Connection error: ", err)
+		return -1
+	}
+	return 0
+}
+
+//export kv739_shutdown
+func kv739_shutdown() int {
+	return 0
+}
+
+func kv739_changeMembership(newConfig []int) int {
+	return executeChangeMembership(newConfig, serverList[leaderIndex])
+}
+
+func executeChangeMembership(newConfig []int, address string) int {
+	if len(address) > 0 {
+		conn, err = net.DialTimeout("tcp", address, 250*time.Millisecond)
+	}
+	if err == nil {
+		client = rpc.NewClient(conn)
+		defer client.Close()
+		defer conn.Close()
+		conn.SetDeadline(time.Now().Add(250 * time.Millisecond))
+		var reply int
+		err = client.Call("RaftServer.ChangeMembership", newConfig, &reply)
+		if err == nil {
+			return 0
+		} else if match, _ := regexp.MatchString(".*LeaderIndex.*", err.Error()); match {
+			thisLeader, _ := strconv.Atoi(strings.Split(err.Error(), "LeaderIndex:")[1])
+			if thisLeader != -1 {
+				leaderIndex = thisLeader
+			}
+			address = serverList[leaderIndex]
+			return executeChangeMembership(newConfig, address)
+		} else if match, _ := regexp.MatchString(".*connection.*", err.Error()); match {
+			leaderIndex = (leaderIndex + 1) % len(serverList)
+			getResult := executeChangeMembership(newConfig, serverList[leaderIndex])
+			if getResult != -1 {
+				return getResult
+			}
+		}
+		fmt.Println("Unable to change membership: ", newConfig, " from leader: ", leaderIndex, " err: ", err)
+	} else if match, _ := regexp.MatchString(".*connection.*", err.Error()); match {
+		leaderIndex = (leaderIndex + 1) % len(serverList)
+		getResult := executeChangeMembership(newConfig, serverList[leaderIndex])
+		if getResult != -1 {
+			return getResult
+		}
+	}
+	fmt.Println("Error in change membership", err)
+	return -1
+}
